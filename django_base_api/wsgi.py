f08f9a7c609e100ac68f030ac7bc9b19d01e43d2
Alexandru Dragomir <a-dragomir@outlook.com>
2024-12-26 15:30:16 -0500

    renamed project

--- django_base_api/wsgi.py
+++ django_base_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for megaparse project.
+WSGI config for django_base_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megaparse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_base_api.settings')
 
 application = get_wsgi_application()
