ab7ad7fa3aa0683ae735c0d135db9a2b147f8658
Alexandru Dragomir <a-dragomir@outlook.com>
2024-12-26 15:13:38 -0500

    updated navbar and added empty pages

--- megaparse/urls.py
+++ megaparse/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+
+from . import views
 from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),  # Root URL pointing to the home_view
+    path('page-one/', views.page_one, name='page_one'),
+    path('page-two/', views.page_two, name='page-two')
 ]
--- megaparse/views.py
+++ megaparse/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home.html')
+
+def page_one(request):
+    return render(request, 'page_one.html')
+
+
+def page_two(request):
+    return render(request, 'page_two.html')
