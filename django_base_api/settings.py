eed20b1288507b4236c5938fefc59b402efd8c19
Alexandru Dragomir <a-dragomir@outlook.com>
2024-12-27 00:06:31 -0500

    updated allowed hosts

--- django_base_api/settings.py
+++ django_base_api/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-)w(j_m!@#fvp#xebql-eca-!@+v#rccypya6e=uls*1i%q@h=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', '67.68.141.30', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
